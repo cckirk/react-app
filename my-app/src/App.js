33506d5c56d9f0425e5dd5d58a661c96d6a39b8a
unknown <ckirk0527@windstream.net>
2023-05-05 12:40:33 -0700

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Header from './components/Header';
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header name="Anna" color="purple" />
+      <Main greet="Howdy"/>
+      <Sidebar greet="Hi" />
+    </div>
+  )
+}
+
+export default App;
